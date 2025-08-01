9d3f5a106f9e42bba5c422ec15d9c4834669bad1
soenderskov <antonjimmyheino@gmail.com>
2025-08-01 09:08:38 +0200

    fixed the bugs in main and json files

--- json.go
+++ json.go
@@ -29,6 +29,11 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		w.WriteHeader(500)
 		return
 	}
+
 	w.WriteHeader(code)
-	w.Write(dat)
+	if _, err := w.Write(dat); err != nil {
+		// Log or handle the error
+		log.Printf("failed to write response: %v", err)
+	}
+
 }
